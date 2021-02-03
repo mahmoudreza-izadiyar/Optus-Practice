c65e7834a166e22cab713aa266ede56834d8ae00
Mahmoudreza-Izadiyar <m.izadiyar@gmail.com>
2021-02-03 15:37:47 +1100

    All Pages are ready to luch the model

--- OptusPractice/cars/views.py
+++ OptusPractice/cars/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {
+        "data": "Hello lord Mahmoud"
+    }
+    return render(request, "cars/index.html", context)
